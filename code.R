0218b1a58dc5cb36aa362f2e2a158a1abcb0f3c1
Valexandre <Valexandre@users.noreply.github.com>
2020-10-08 11:20:55 +0200

    Update code.R

--- code.R
+++ code.R
@@ -71,6 +71,20 @@ polys = SpatialPolygons(
   )
 )
 
+ggmap_rast <- function(map){
+  map_bbox <- attr(map, 'bb') 
+  .extent <- extent(as.numeric(map_bbox[c(2,4,1,3)]))
+  my_map <- raster(.extent, nrow= nrow(map), ncol = ncol(map))
+  rgb_cols <- setNames(as.data.frame(t(col2rgb(map))), c('red','green','blue'))
+  red <- my_map
+  values(red) <- rgb_cols[['red']]
+  green <- my_map
+  values(green) <- rgb_cols[['green']]
+  blue <- my_map
+  values(blue) <- rgb_cols[['blue']]
+  stack(red,green,blue)
+}
+
 polys_sf<-st_as_sf(polys)
 polys_sf%>%ggplot()+geom_sf(colour="red")  
   
